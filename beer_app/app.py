df53f3394d702869574f0f727e1a1d9dad9fbf6e
Lhosb <lhosb95@mac.com>
2020-09-17 13:06:06 -0700

    initial api

---
+++ beer_app/app.py
@@ -0,0 +1,36 @@
+# Reads dht22 sensor and saves to database
+
+import sqlite3
+import sys
+import board
+import time
+import adafruit_dht
+
+SENSOR_LOCATION_NAME = "Potter_Closet"
+DEVICE = "DHT22"
+SLEEP_TIME = 3600
+dht = adafruit_dht.DHT22(board.D4)
+
+def log_values(device, location, temp, hum):
+    conn=sqlite3.connect('./sensordata.db')  
+    curs=conn.cursor()
+    curs.execute("""INSERT INTO dhtreadings(currentdate, currenttime, device, location, temperature, humidity) values(date('now'), time('now'),
+         (?), (?), (?), (?))""", (device, location, temp, hum))
+    conn.commit()
+    conn.close()
+
+while True:
+    try: 
+        humidity = dht.humidity
+        temperature = dht.temperature
+        
+        if humidity is not None and temperature is not None:
+            print(humidity)
+            print(temperature)
+            log_values(DEVICE, SENSOR_LOCATION_NAME, temperature, humidity)
+        else:
+            log_values(DEVICE, SENSOR_LOCATION_NAME, -999, -999)
+        
+    except RuntimeError as error:
+        print(error.args[0])
+    time.sleep(SLEEP_TIME)
